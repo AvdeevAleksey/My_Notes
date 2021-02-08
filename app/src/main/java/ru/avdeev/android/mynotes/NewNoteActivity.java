213cf9471875ec69675364bc1f088b1f6953c2d9
AvdeevAleksey <avdeev.hexer@yandex.ru>
2021-02-08 17:14:07 +0300

    Implemented a dialog for selecting the date and time.

--- app/src/main/java/ru/avdeev/android/mynotes/NewNoteActivity.java
+++ app/src/main/java/ru/avdeev/android/mynotes/NewNoteActivity.java
@@ -1,18 +1,33 @@
 package ru.avdeev.android.mynotes;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Calendar;
+
 public class NewNoteActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editTextDateTime;
+    ImageButton buttonCalendar;
+    CheckBox deadlineChkBox;
+    Calendar todayCalendar = Calendar.getInstance();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,12 +44,44 @@ public class NewNoteActivity extends AppCompatActivity implements View.OnClickLi
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         editTextDateTime = (EditText) findViewById(R.id.deadlineEditText);
-        editTextDateTime.setOnClickListener(this);
-    }
+        buttonCalendar = (ImageButton) findViewById(R.id.btnCalendar);
+        deadlineChkBox = (CheckBox) findViewById(R.id.deadlineCheckBox);
 
+        deadlineChkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    editTextDateTime.setFocusable(true);
+                    editTextDateTime.setLongClickable(true);
+                    editTextDateTime.setCursorVisible(true);
+                    buttonCalendar.setLongClickable(true);
+                    buttonCalendar.setFocusable(true);
+                    editTextDateTime.setOnClickListener(NewNoteActivity.this::onClick);
+                    buttonCalendar.setOnClickListener(NewNoteActivity.this::onClick);
+                } else {
+                    editTextDateTime.setFocusable(false);
+                    editTextDateTime.setLongClickable(false);
+                    editTextDateTime.setCursorVisible(false);
+                    buttonCalendar.setLongClickable(false);
+                    buttonCalendar.setFocusable(false);
+                }
+            }
+        });
+    }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.deadlineEditText: {
+                setInitialDateTime();
+                break;
+            }
+            case R.id.btnCalendar: {
+                setTime(view);
+                setDate(view);
+                break;
+            }
+        }
 
     }
 
@@ -42,30 +89,60 @@ public class NewNoteActivity extends AppCompatActivity implements View.OnClickLi
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.notesSave: {
-                //  Здесь код по сохранению заметки, возвращению в NotesActivity,
+                //  Здесь код по сохранению заметки, возвращение в NotesActivity,
                 //  пересоздание списка, вывод сообщения об успешном сохранении.
-            }
-            case R.id.deadlineEditText: {
-                //  Здесь код возвращает текущую дату и время для дедлайна.
-                //  Как-то надо впихнуть этот код сюда
-                //
-                //  Calendar todayCalendar = Calendar.getInstance();
-                //      datePickerDialog = new DatePickerDialog(
-                //        this,
-                //        onDateSet,
-                //        todayCalendar.get(Calendar.YEAR),
-                //        todayCalendar.get(Calendar.MONTH),
-                //        todayCalendar.get(Calendar.DAY_OF_MONTH)
-                //      );
-                //  datePickerDialog.show();
             }
         }
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private void setInitialDateTime() {
+        editTextDateTime.setText(DateUtils.formatDateTime(this,
+                todayCalendar.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
+    }
+
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            todayCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            todayCalendar.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+        }
+    };
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            todayCalendar.set(Calendar.YEAR, year);
+            todayCalendar.set(Calendar.MONTH, monthOfYear);
+            todayCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
+
+    // отображаем диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(NewNoteActivity.this, d,
+                todayCalendar.get(Calendar.YEAR),
+                todayCalendar.get(Calendar.MONTH),
+                todayCalendar.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    // отображаем диалоговое окно для выбора времени
+    public void setTime(View v) {
+        new TimePickerDialog(NewNoteActivity.this, t,
+                todayCalendar.get(Calendar.HOUR_OF_DAY),
+                todayCalendar.get(Calendar.MINUTE), true)
+                .show();
     }
 }
